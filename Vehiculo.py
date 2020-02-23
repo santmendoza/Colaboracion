2aea3f048741d20e7e4f5e4e0bc99eccc7645884
santmendoza <santiagomendoza1997@gmail.com>
2020-02-23 16:17:46 -0500

    He creado un constructor

---
+++ Vehiculo.py
@@ -0,0 +1,17 @@
+
+
+class Vehiculo:
+        pass
+        def __init__(self,nombre,ruedas,color,modelo):
+            self.nombre = nombre
+            self.ruedas = ruedas
+            self.color =color
+            self.modelo=modelo 
+
+
+        
+        def arrancar(self,nombre):
+            return('El carro'+nombre+'ha arrancado')
+
+            
+
