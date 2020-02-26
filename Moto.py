77f01af2068561b2556f72c7c35118930602e32d
cristianmercado1997 <47404910+cristianmercado1997@users.noreply.github.com>
2020-02-26 15:21:17 -0500

    otro mande

--- Moto.py
+++ Moto.py
@@ -2,9 +2,10 @@ from Vehiculo import Vehiculo
 
 class Moto(Vehiculo):
     #LLamamos al constructor del padre y le asignamos el nuevo atributo
-    def __init__(self,nombre,ruedas,color,modelo,cilindraje):
+    def __init__(self,nombre,ruedas,color,modelo,cilindraje,mover):
         #Luego creamos el nuevo constructor con el nuevo atributo
         Vehiculo.__init__ (self,nombre,ruedas,color,modelo)
+        self.mover=mover
         self.cilindraje=cilindraje
 
 
@@ -14,14 +15,5 @@ class Moto(Vehiculo):
     def frenar(self,nombre,color):
         return print('La moto de nombre'+nombre+' ha frenado! de color'+color)
 
-    def acerlerar(self):
-        pass
-
-    
-
-
-
-
-
-
-        
+    def acerlerar(self,mover,fondear): 
+        return mover=mover+fondear
